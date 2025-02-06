c5cb69a257a4d5a19a9022283aa9d09f1f54a888
tomgodsalo <tomgodsalo1@gmail.com>
2025-02-06 12:57:36 +0530

    f1

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { HashRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+
+const App = () => {
+  return (
+
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar />
+        <Hero />
+      </div>
+      <Works />
+
+
+      {/* <About /> */}
+      <div className='relative z-0'>
+        <Experience />
+        <Tech />
+        <StarsCanvas />
+      </div>
+
+      {/* <Feedbacks /> */}
+      {/* <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div> */}
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <Routes>
+      <Route path="*" element={<App />}>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+  // </React.StrictMode>
+);
+
+// {
+//   <BrowserRouter basename={import.meta.env.BASE_URL}>
+//   <Routes>
+//     <Route path="*" element={<App />}>
+//     </Route>
+//   </Routes>
+// </BrowserRouter>
+// }
